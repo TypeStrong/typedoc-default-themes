ed97161f1ef433fe4f0cf88f7bcb3ee16d0f702f
Ricardo De Peña <ricardo@isnull.co>
2019-02-11 15:32:27 -0500

    Removed grunt-contrib-sass in favor of grunt-sass (#56)

--- gruntfile.js
+++ gruntfile.js
@@ -149,7 +149,7 @@ module.exports = function(grunt)
 
 
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-string-replace');
