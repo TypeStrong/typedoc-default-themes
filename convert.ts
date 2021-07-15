b43c481d49a7595087e728e7813d482f724c4fb1
Andrew Bradley <cspotcode@gmail.com>
2021-07-15 01:30:29 -0400

    update

--- convert.ts
+++ convert.ts
@@ -1,12 +1,17 @@
 import {readFileSync, writeFileSync} from 'fs';
 import { compile } from 'handlebars-to-jsx';
 import {sync as globSync} from 'glob';
+import {outdent} from 'outdent';
 
 for(const file of globSync('src/**/*.hbs')) {
     console.dir(file);
     try {
         const converted = compile(readFileSync(file, 'utf8'));
-        writeFileSync(file.replace(/.hbs$/, '.ts'), converted);
+        writeFileSync(file.replace(/.hbs$/, '.tsx'), outdent `
+            import React from 'react';
+            export const component = ${ converted }
+
+        `);
     } catch(e) {
         // if(e.message.includes('block may only')) {
             console.log(e.message);
